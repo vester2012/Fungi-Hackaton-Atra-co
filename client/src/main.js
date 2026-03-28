cab33b07ea37b471b09b45bb745566ed60c1f985
Anton <workglebov@gmail.com>
2026-03-28 18:05:47 +0300

    enable eruda

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,19 @@ import { io } from "socket.io-client";
 import { bootGame } from './game/bootGame.js';
 import {unit_manager} from "./game/unit_manager.js";
 
+import('eruda').then((eruda) => {
+    eruda.default.init();
+
+    const entry = document.querySelector('.eruda-entry-btn');
+
+    if (entry) {
+        entry.style.left = 'auto';
+        entry.style.right = '10vw';
+        entry.style.top = '10vh';
+        entry.style.bottom = 'auto';
+    }
+});
+
 async function startGame() {
     if (document.fonts?.load) {
         await document.fonts.load('16px "JungleAdventurer"');
@@ -66,6 +79,5 @@ socket.on('hpUpdate', (data) => {
         }
     }
 });
-
 
 unit_manager.socket = socket;
