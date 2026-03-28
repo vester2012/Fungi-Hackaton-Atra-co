a55af6ae25f79c82cc08f4041f5d77ef4552011e
Vladislav <mistervladik444@gmail.com>
2026-03-28 13:55:42 +0300

    add socket

---
+++ client/src/game/unit_manager.js
@@ -0,0 +1,6 @@
+export const unit_manager = {
+    info : {
+        players: []
+    }
+};
+
--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,40 @@
 import './style.css';
+import { io } from "socket.io-client";
 import { bootGame } from './game/bootGame.js';
+import {unit_manager} from "./game/unit_manager.js";
 
 bootGame('app');
 
+
+
+//* SOCKET *//
+
+const socket = io();
+
+let players = unit_manager.info.players;
+let myId = null;
+
+socket.on('connect', () => { myId = socket.id; });
+
+// Получаем список всех игроков при входе
+socket.on('currentPlayers', (serverPlayers) => {
+    Object.assign(players, serverPlayers);
+});
+
+// Новый игрок зашел
+socket.on('newPlayer', (playerInfo) => {
+    players[playerInfo.id] = playerInfo;
+});
+
+// Кто-то передвинулся
+socket.on('playerMoved', (playerInfo) => {
+    if (players[playerInfo.id]) {
+        players[playerInfo.id].x = playerInfo.x;
+        players[playerInfo.id].y = playerInfo.y;
+    }
+});
+
+// Удаление вышедшего игрока
+socket.on('playerDisconnected', (playerId) => {
+    delete players[playerId];
+});
