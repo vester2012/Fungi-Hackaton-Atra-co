8c68055c33687edcdad37d798b8452caa1b26928
Anton <workglebov@gmail.com>
2026-03-28 17:42:31 +0300

    fix isMobile

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,10 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const isMobile = window?.matchMedia("(max-width: 768px)").matches ?? /Mobi|Android/i.test(navigator.userAgent)
+const isMobile =
+    typeof window !== 'undefined' &&
+    (window.matchMedia("(max-width: 768px)").matches ||
+        /Mobi|Android/i.test(navigator.userAgent));
 
 const PORT = process.env.PORT || 3000;
 const PATH_PUBLIC = path.resolve(__dirname, '../../client/dist');
